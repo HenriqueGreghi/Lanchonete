b6bc8e3bb5b5dcde31dd24ae170979212aa4dfb9
Henrique Greghi <henriquevsg@gmail.com>
2022-08-12 11:35:15 -0300

    Add classe calculo

---
+++ Lanchonete/Calculo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Lanchonete
+{
+    class Calculo
+    {
+        public double Quantidade;
+        public double Produto;
+        public double Total;
+       
+        public double ValorPedido()
+        {
+            Total = Quantidade * Produto;
+
+            return Total;
+        }
+        public override string ToString()
+        {
+           return"Total : R$" + ValorPedido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
--- Lanchonete/Program.cs
+++ Lanchonete/Program.cs
@@ -5,20 +5,22 @@ namespace Lanchonete
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-
 
             Console.WriteLine("1 - Cachorro Quente.");
             Console.WriteLine("2 - X-Salada.");
             Console.WriteLine("3 - X-Bacon. ");
             Console.WriteLine("4 - Torrada Simples. ");
             Console.WriteLine("5 - Refrigerante.");
-            Console.WriteLine("6 - Sair.");
+            Console.WriteLine("6 - X-Dog.");
+            Console.WriteLine("7 - Sair.");
             Console.WriteLine();
             Console.Write("Escolha uma opição: ");
 
             int x = int.Parse(Console.ReadLine());
+
 
             if (x == 1)
             {
@@ -41,6 +43,10 @@ namespace Lanchonete
             {
                 Refrigerante();
             }
+            else if (x == 6)
+            {
+                XDog();
+            }
             else
             {
 
@@ -48,53 +54,65 @@ namespace Lanchonete
 
 
 
+
         }
         public static void CachorroQuente()
         {
-            double valor = 4.00;
+            Calculo l = new Calculo();
+            l.Produto = 4.00;
             Console.Write("Entre com a quantidade: ");
-            double quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            l.Quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double total = valor * quantidade;
-            Console.WriteLine("Total : R$" + total.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(l);
+
         }
         public static void XSalada()
         {
-            double valor = 4.50;
+            Calculo l = new Calculo();
+            l.Produto = 4.50;
             Console.Write("Entre com a quantidade: ");
-            double quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            l.Quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double total = valor * quantidade;
-            Console.WriteLine("Total : R$" + total.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(l);
 
         }
         public static void XBacon()
         {
-            double valor = 5.00;
+            Calculo l = new Calculo();
+            l.Produto = 5.00;
             Console.Write("Entre com a quantidade: ");
-            double quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            l.Quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double total = valor * quantidade;
-            Console.WriteLine("Total : R$" + total.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(l);
 
         }
         public static void TorradaSimples()
         {
-            double valor = 2.00;
+            Calculo l = new Calculo();
+            l.Produto = 2.00;
             Console.Write("Entre com a quantidade: ");
-            double quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            l.Quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double total = valor * quantidade;
-            Console.WriteLine("Total : R$" + total.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(l);
         }
         public static void Refrigerante()
         {
-            double valor = 1.50;
+            Calculo l = new Calculo();
+            l.Produto = 1.50;
             Console.Write("Entre com a quantidade: ");
-            double quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            l.Quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double total = valor * quantidade;
-            Console.WriteLine("Total : R$" + total.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(l);
+        }
+        public static void XDog()
+        {
+            Calculo l = new Calculo();
+            l.Produto = 5.50;
+            Console.Write("Entre com a quantidade: ");
+            l.Quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(l);
+
         }
     }
 }
